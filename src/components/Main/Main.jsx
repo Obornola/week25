328044b0bfc87abac6904ab9baf6f26e5d5642db
Olga Chirkova <obornola@gmai.com>
2022-11-13 01:18:13 +0300

    add homework week 25

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Chart } from "react-google-charts";
+
+export const data = [
+    ["Task", "Hours per Day"],
+    ["Соцсети", 4],
+    ["Фоновая музыка", 2.5],
+    ["Тестирование", 0.5],
+    ["Залипание в экран", 3],
+    ["Повторное тестирование, даже если уже всё работает", 1.5],
+    ["Написание кода", 1.5],
+  ];
+  
+  export const options = {
+    title: "Время, затраченное на программирование",
+  };
+
+export default function Main() {
+    return (
+        <div>
+<Chart
+  chartType="PieChart"
+  data={data}
+  options={options}
+  width={"100%"}
+  height={"400px"}
+/>
+        </div>
+    );
+}
+
